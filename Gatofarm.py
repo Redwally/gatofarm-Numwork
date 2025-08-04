e8a4c5bd1579cfa0a62f8a9910c81388c7b82330
Redwally <74724871+Redwally@users.noreply.github.com>
2025-08-04 14:47:48 +0200

    Update Gatofarm.py
    stabilité + commentaire pour facilité le dev

--- Gatofarm.py
+++ Gatofarm.py
@@ -1,69 +1,81 @@
-from math import *
-from time import *
-from kandinsky import *
-from ion import *
+from time import monotonic, sleep
+from kandinsky import fill_rect, draw_string
+from ion import keydown, KEY_ONE, KEY_TWO, KEY_THREE, KEY_ZERO, KEY_EXE
 
 menu = 0
-click = 1
+click = 0.0
 click_interval = 0.2
-auto_click = 0.1
-activate_autoclik = True
+auto_click_rate = 0.1
+autoclick_active = True
 
-def autoclick():
-    global click
-    global auto_click
-    if activate_autoclik is True:
+last_click_time = 0
+last_autoclick_time = 0
 
-        click += auto_click  # Incrémente click avec auto_click
-        click = round(click, 1)  # Arrondi à 1 chiffre après la virgule
-        click = int(click * 10) / 10  # Forcer à garder seulement un chiffre après la virgule
-        sleep(0.5)  # Délai d'une seconde
+def clear_screen():
+    fill_rect(0, 0, 320, 240, "white")
 
-
-    
-
-
-
-
-        
-
-def sup():
-    fill_rect(1, 1, 320, 240, "white")  # Efface l'écran
+clear_screen()
 
 while True:
-    if menu == 0:
-        sleep(0.2)
-        draw_string("[1]-clicker", 100, 50)
-        draw_string("[2]-stats", 100, 70)
-        draw_string("[3]-quit", 100, 90)
+    now = monotonic()
 
-        if keydown(KEY_ONE):  # Pas besoin de "== True", `keydown` renvoie déjà un booléen
+    if menu == 0:
+        clear_screen()
+        draw_string("[1] Clicker", 100, 50)
+        draw_string("[2] Stats", 100, 70)
+        draw_string("[3] Quitter", 100, 90)
+        sleep(0.2)
+
+        if keydown(KEY_ONE):
             menu = 1
-            sup()
+            clear_screen()
+            sleep(0.2)
         elif keydown(KEY_TWO):
             menu = 2
-            sup()
+            clear_screen()
+            sleep(0.2)
         elif keydown(KEY_THREE):
             break
-    
-    if menu == 1:
-        autoclick()
-        draw_string("click: "+str(click), 10, 10)
-        draw_string("Auto_click:"+str(auto_click),10,30)
-        draw_string("Auto_click:"+str(activate_autoclik),10,50)
-        draw_string("[1]-Activer/desactiver autoclick",10,170)
-        draw_string("[0]-Home menu",10,200)
+
+    elif menu == 1:
+        # Autoclick toutes les 0.5 secondes
+        if autoclick_active and now - last_autoclick_time > 0.5:
+            click += auto_click_rate
+            click = int(click * 10) / 10
+            last_autoclick_time = now
+
+        # Click manuel avec intervalle
+        if keydown(KEY_EXE) and now - last_click_time > click_interval:
+            click += 1
+            last_click_time = now
+
+        # Toggle autoclick
+        if keydown(KEY_ONE):
+            autoclick_active = not autoclick_active
+            sleep(0.3)  # anti-rebond
+
+        # Retour menu
         if keydown(KEY_ZERO):
             menu = 0
-            sup()
-        if keydown(KEY_EXE):
-            click= click+1
-            sleep(click_interval)
-        
-        if keydown(KEY_ONE):
-            if activate_autoclik is False:
-                activate_autoclik = bool(True)
-                sleep(0.5)
-            else:
-                activate_autoclik = bool(False)
-                sleep(0.5)
+            clear_screen()
+            sleep(0.3)
+
+        clear_screen()
+        draw_string("Clicks : " + str(click), 10, 10)
+        draw_string("Auto_click : " + str(auto_click_rate), 10, 30)
+        draw_string("Auto ON : " + str(autoclick_active), 10, 50)
+        draw_string("[EXE] +1 Click", 10, 120)
+        draw_string("[1] Toggle AutoClick", 10, 140)
+        draw_string("[0] Retour menu", 10, 200)
+
+        sleep(0.05)
+
+    elif menu == 2:
+        clear_screen()
+        draw_string("Stats non implémentées.", 50, 100)
+        draw_string("[0] Retour menu", 50, 130)
+        sleep(0.2)
+        if keydown(KEY_ZERO):
+            menu = 0
+            clear_screen()
+            sleep(0.3)
